4235db7c0f012135606231640c3f78f186ec8153
ccsexyz <mofaxyz@gmail.com>
2019-07-16 10:49:26 +0800

    add libuv demo and multi_curl

---
+++ multi_curl.c
@@ -0,0 +1,64 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <curl/curl.h>
+ 
+static size_t write_cb(char *data, size_t n, size_t l, void *userp)
+{
+  /* take care of the data here, ignored in this example */ 
+  (void)data;
+  (void)userp;
+  return n*l;
+}
+ 
+static void add_transfer(CURLM *cm, const char *url)
+{
+  CURL *eh = curl_easy_init();
+  curl_easy_setopt(eh, CURLOPT_WRITEFUNCTION, write_cb);
+  curl_easy_setopt(eh, CURLOPT_URL, url);
+  curl_easy_setopt(eh, CURLOPT_PRIVATE, url);
+  curl_multi_add_handle(cm, eh);
+}
+ 
+int main(int argc, char **argv)
+{
+  CURLM *cm;
+  CURLMsg *msg;
+  unsigned int transfers = 0;
+  int msgs_left = -1;
+  int still_alive = 1;
+ 
+  curl_global_init(CURL_GLOBAL_ALL);
+  cm = curl_multi_init();
+ 
+  for(transfers = 1; transfers < argc; transfers++)
+    add_transfer(cm, (const char *)argv[transfers]);
+ 
+  do {
+    curl_multi_perform(cm, &still_alive);
+ 
+    while((msg = curl_multi_info_read(cm, &msgs_left))) {
+      if(msg->msg == CURLMSG_DONE) {
+        char *url;
+        CURL *e = msg->easy_handle;
+        curl_easy_getinfo(msg->easy_handle, CURLINFO_PRIVATE, &url);
+        fprintf(stderr, "R: %d - %s <%s>\n",
+                msg->data.result, curl_easy_strerror(msg->data.result), url);
+        curl_multi_remove_handle(cm, e);
+        curl_easy_cleanup(e);
+      }
+      else {
+        fprintf(stderr, "E: CURLMsg (%d)\n", msg->msg);
+      }
+    }
+    if(still_alive)
+      curl_multi_wait(cm, NULL, 0, 1000, NULL);
+ 
+  } while(still_alive);
+ 
+  curl_multi_cleanup(cm);
+  curl_global_cleanup();
+ 
+  return EXIT_SUCCESS;
+}
---
+++ uv_echo.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <uv.h>
+
+typedef struct {
+    uv_write_t req;
+    uv_buf_t buf;
+} write_req_t;
+
+static uv_loop_t *default_loop;
+
+static void on_close(uv_handle_t *handle) {
+    free(handle);
+}
+
+static void after_shutdown(uv_shutdown_t *req, int status) {
+    uv_close((uv_handle_t *)req->handle, on_close);
+    free(req);
+}
+
+static void after_write(uv_write_t *req, int status) {
+    write_req_t *wr = (write_req_t *)req;
+
+    free(wr->buf.base);
+    free(wr);
+
+    if (status == 0) {
+        return;
+    }
+
+    if (status == UV_ECANCELED) {
+        return;
+    }
+
+    uv_close((uv_handle_t *)req->handle, on_close);
+}
+
+static void after_read(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf) {
+    if (nread <= 0 && buf->base) {
+        free(buf->base);
+    }
+
+    if (nread == 0) {
+        return;
+    }
+
+    if (nread < 0) {
+        uv_shutdown_t *req = (uv_shutdown_t *)malloc(sizeof(uv_shutdown_t));
+        assert(req);
+
+        int rc = uv_shutdown(req, handle, after_shutdown);
+        assert(rc == 0);
+
+        return;
+    }
+
+    write_req_t *wr = (write_req_t *)malloc(sizeof(write_req_t));
+    assert(wr);
+
+    wr->buf = uv_buf_init(buf->base, nread);
+
+    int rc = uv_write(&wr->req, handle, &wr->buf, 1, after_write);
+    assert(rc == 0);
+}
+
+static void alloc_cb(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+    buf->base = malloc(suggested_size);
+    assert(buf->base);
+    buf->len = suggested_size;
+}
+
+static void on_new_connection(uv_stream_t *server, int status) {
+    assert(status == 0);
+
+    uv_tcp_t *stream = calloc(1, sizeof(uv_tcp_t));
+    assert(stream);
+
+    int rc = uv_tcp_init(default_loop, stream);
+    assert(rc == 0);
+
+    stream->data = server;
+
+    rc = uv_accept(server, (uv_stream_t *)stream);
+    assert(rc == 0);
+
+    rc = uv_read_start((uv_stream_t *)stream, alloc_cb, after_read);
+    assert(rc == 0);
+}
+
+static int create_tcp_echo_server(uv_loop_t *loop, int port) {
+    struct sockaddr_in addr;
+
+    int rc = uv_ip4_addr("127.0.0.1", port, &addr);
+    if (rc != 0) {
+        return rc;
+    }
+
+    uv_tcp_t *server = (uv_tcp_t *)calloc(1, sizeof(uv_tcp_t));
+    assert(server);
+
+    rc = uv_tcp_init(loop, server);
+    if (rc != 0) {
+        free(server);
+        return rc;
+    }
+
+    rc = uv_tcp_bind(server, (const struct sockaddr *)&addr, 0);
+    assert(rc == 0);
+
+    rc = uv_listen((uv_stream_t *)server, 512, on_new_connection);
+    assert(rc == 0);
+
+    return 0;
+}
+
+int main(void) {
+    uv_loop_t *loop = uv_default_loop();
+    assert(loop);
+    default_loop = loop;
+
+    int rc = create_tcp_echo_server(loop, 8090);
+    assert(rc == 0);
+
+    rc = uv_run(loop, UV_RUN_DEFAULT);
+    assert(rc == 0);
+
+    printf("Hello World! loop = %p\n", loop);
+    return 0;
+}
+
