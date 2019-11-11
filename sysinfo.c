8234fa306fdfa6fcd8cf2ce16112087a5a18451f
ccsexyz <mofaxyz@gmail.com>
2019-11-11 13:23:43 +0800

    [sysinfo] print stack size

--- sysinfo.c
+++ sysinfo.c
@@ -4,5 +4,14 @@ int
 main(void)
 {
     printf("pagesize = %d\n", getpagesize());
+
+    {
+        size_t stacksize;
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+        pthread_attr_getstacksize(&attr, &stacksize);
+        printf("pthread stack size = %zu bytes \n", stacksize);
+    }
+
     return 0;
 }
